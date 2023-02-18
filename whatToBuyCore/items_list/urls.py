bb58fc0e3607d07a9f853288995d998ffc311590
EimantasBlazevicius <eima.blaz@gmail.com>
2023-02-18 08:50:38 +0200

    prep

--- whatToBuyCore/items_list/urls.py
+++ whatToBuyCore/items_list/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import ItemCreateView, ItemUpdateView, ItemDeleteView, my_lists, my_list
+from .views import ItemCreateView, ItemUpdateView, ItemDeleteView, my_lists, SingleListView
 
 urlpatterns = [
     path("my_lists/", my_lists, name="items_lists"),
-    path("my_lists/<int:pk>/", my_list, name="items_list"),
+    path("my_lists/<int:pk>/", SingleListView.as_view(), name="items_list"),
     path('items/add/', ItemCreateView.as_view(), name='item_add'),
     path('items/<int:pk>/update', ItemUpdateView.as_view(), name='item_update'),
     path('items/<int:pk>/delete/',
--- whatToBuyCore/items_list/views.py
+++ whatToBuyCore/items_list/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.views.generic.list import ListView
 from .models import ShoppingList, ShoppingItem
 
 from django.urls import reverse_lazy
@@ -25,8 +26,9 @@ def my_lists(request):
     return render(request, "shopping_lists.html",  context={"shopping_lists": shopping_lists})
 
 
-def my_list(request, pk):
+class SingleListView(ListView):
+    def get(self, request, pk):
 
-    shopping_list = ShoppingList.objects.get(id=pk)
-    items_list = shopping_list.shoppingitem_set.all()
-    return render(request, "shopping_list.html",  context={"shopping_list": items_list})
+        shopping_list = ShoppingList.objects.get(id=pk)
+        items_list = shopping_list.shoppingitem_set.all()
+        return render(request, "shopping_list.html",  context={"shopping_list": items_list})
