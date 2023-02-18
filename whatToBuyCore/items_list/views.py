947e7eb42c0086c2e36f675477b88fa696c7a77a
EimantasBlazevicius <eima.blaz@gmail.com>
2023-02-18 11:47:49 +0200

    create templates, add form functionality and delete functionality

--- whatToBuyCore/items_list/views.py
+++ whatToBuyCore/items_list/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import ShoppingList
+from .models import ShoppingList, ShoppingItem
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+
+class ItemCrateView(CreateView):
+    model = ShoppingItem
+    fields = '__all__'
+
+
+class ItemUpdateView(UpdateView):
+    model = ShoppingItem
+    fields = '__all__'
+
+
+class ItemDeleteView(DeleteView):
+    model = ShoppingItem
+    success_url = reverse_lazy("lists-list")
 
 
 def my_lists(request):
@@ -8,8 +24,8 @@ def my_lists(request):
     return render(request, "shopping_lists.html",  context={"shopping_lists": shopping_lists})
 
 
-def items_list(request, id):
-    shopping_list = ShoppingList.objects.get(id=id)
+def items_list(request, pk):
+    shopping_list = ShoppingList.objects.get(pk=pk)
     # shoppingitem_set is derived from modelname_set
     items_list = shopping_list.shoppingitem_set.all()
     return render(request, "shopping_list.html",  context={"shopping_list": items_list})
